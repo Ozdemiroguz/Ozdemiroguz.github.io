c6aa8858d3e456ce3a05a1ed9099081255ee551d
Oğuzhan Özdemir <ozdemiroguzhan55@gmail.com>
2022-08-29 23:17:00 +0300

    New Website

--- scriptElif.js
+++ scriptElif.js
@@ -1,13 +1,3 @@
-$(function () {
-    $("a[href*=#]").on("click", function (e) {
-        e.preventDefault();
-        $("html, body").animate(
-            { scrollTop: $($(this).attr("href")).offset().top },
-            500,
-            "linear"
-        );
-    });
-});
 document.querySelector('.header__moon').onclick = function () {
     document.querySelector('body').classList.toggle('is-black');
 }
