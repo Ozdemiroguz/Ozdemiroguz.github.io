944eb6d48f58ed8d6ede2c87bcc9b8c2f1b7dbd2
Oğuzhan Özdemir <ozdemiroguzhan55@gmail.com>
2022-12-14 20:59:39 +0300

    add a new game

--- scriptElif.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.header__moon').onclick = function () {
-    document.querySelector('body').classList.toggle('is-black');
-}
-
-document.querySelector('.header__mob-menu').onclick = function () {
-    document.querySelector('.header-menu').classList.toggle('is-active');
-}
