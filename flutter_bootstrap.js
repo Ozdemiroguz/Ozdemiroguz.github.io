a7f2b906661ef361b4ab71b5398796f751bbed7f
Oğuzhan <oguz@Oguzhans-MacBook-Air.local>
2025-11-03 20:15:09 +0300

    Update: Added web_icon.webp as favicon and updated site title to Oğuzhan Özdemir

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3118829208"
+    serviceWorkerVersion: "740387710"
   }
 });
